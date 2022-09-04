ff3957b1928538ce0eabbe6ddac9611fe42e1a5f
yunhwan <pyh8618@gmail.com>
2022-09-04 22:27:52 +0900

    스프링 예외 추상화를 이용한 예외 처리

---
+++ src/main/java/hello/myjdbc/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package hello.myjdbc.repository;
+
+import hello.myjdbc.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Member findById(String memberId);
+    void update(String memberId, int money);
+    void delete(String memberId);
+}
